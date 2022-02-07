a2a78aa66e9ee49ed75295774f4adcf559b448c5
Alexandre Volts <alexandre.cochet@epitech.eu>
2022-02-07 22:54:14 +0100

    feat([wordle]) language parameter added

--- src/hooks/useDictionary.ts
+++ src/hooks/useDictionary.ts
@@ -2,16 +2,30 @@ import { useEffect, useState } from "react";
 import { useAssets } from "expo-asset";
 import { readAsStringAsync } from "expo-file-system"
 
+const files = [
+    require(`./../../assets/dictionaries/en.txt`),
+    require(`./../../assets/dictionaries/es.txt`),
+    require(`./../../assets/dictionaries/fr.txt`),
+    require(`./../../assets/dictionaries/it.txt`),
+];
+const langs = ["en", "es", "fr", "it"];
 
 export default function useDictionary(lang = "en")
 {
-    const [assets, error] = useAssets([require(`./../../assets/dictionaries/en.txt`)]);
+    const [assets, error] = useAssets(files);
     const [dictionary, setDictionary] = useState(new Array<string>());
 
     useEffect(() => {
-        if (!assets?.[0].localUri)
+        if (error) {
+            console.error(error.message);
+        }
+    }, [error]);
+    useEffect(() => {
+        const uri = assets?.[langs.indexOf(lang)].localUri;
+        
+        if (!uri)
             return;
-        readAsStringAsync(assets[0].localUri)
+        readAsStringAsync(uri)
         .then((data) => setDictionary(data.split("\n")))
         .catch((reason) => console.error(reason));
     }, [assets]);
